56c1667c418b7988fdac1f8fc875912a67e04101
❤️❤️嘉欣❤️ <104952129@qq.com>
2016-07-26 16:08:30 +0800

    add new project

---
+++ 众商智推/Class/Profile/SubClass/Model/WXApiRequestHandler.h
@@ -0,0 +1,17 @@
+//
+//  WXApiRequestHandler.h
+//  众商智推
+//
+//  Created by 杨 on 16/7/26.
+//  Copyright © 2016年 bjywkj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "WXApiObject.h"
+
+
+@interface WXApiRequestHandler : NSObject
+
++ (NSString *)jumpToBizPay;
+
+@end
